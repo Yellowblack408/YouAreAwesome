19ab783848b0cce6f54c9ff47a6faf693da95cce
Mohamed <mohamedyahia408@hotmail.com>
2025-12-15 21:23:36 +0400

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Mohamed Yehia on 15/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

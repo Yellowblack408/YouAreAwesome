1b1cc0e34e5565d183bea1226c6153a82f87125b
Mohamed <mohamedyahia408@hotmail.com>
2025-12-27 21:31:43 +0400

    Adding Some Animation and new tools to the app

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -10,30 +10,50 @@ import SwiftUI
 struct ContentView: View {
     @State private var message = ""
     @State private var imageName = ""
+    @State private var imageNumber = 0
+    @State private var messageNumber = 0
     var body: some View {
         VStack {
-            Spacer()
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
+                .minimumScaleFactor(0.5)
+                .frame(height: 130)
+                .animation(.easeInOut(duration: 0.15), value: message)
+            
             Image(imageName)
                 .resizable()
                 .scaledToFit()
                 .clipShape(RoundedRectangle(cornerRadius: 30))
                 .shadow(radius: 30)
-            Text(message)
-                .font(.largeTitle)
-                .fontWeight(.black)
-                .foregroundStyle(.red)
+                .animation(.default, value: imageName)
             
             Spacer()
             
-            Button("Press Me!") {
-                let message1 = "You Are Awesome!"
-                let message2 = "You Are Great!"
-                let imageName1 = "image0"
-                let imageName2 = "image1"
+            Button("Show Message") {
+                let messages = ["You Are Awesome!",
+                                "You Are Amazing!",
+                                "You Are Fantastic!",
+                                "You Are Incredible!",
+                                "You Are Wonderful!"]
                 
-
-                message = ( message == message1 ? message2 : message1 )
-                imageName = ( imageName == imageName1 ? imageName2 : imageName1 )
+                message = messages[messageNumber]
+                messageNumber += 1
+                
+                if messageNumber == messages.count {
+                    messageNumber = 0
+                }
+                
+                //TODO: Update the imageName variable
+                imageNumber += 1
+                
+                if imageNumber > 9 {
+                    imageNumber = 0
+                }
+                
+                imageName = "image\(imageNumber)"
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
